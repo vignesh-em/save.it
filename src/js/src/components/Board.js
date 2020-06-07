3cc8786c7438fbb41256225f288d453fb1f31fb3
Vignesh <emvignesh16@gmail.com>
2020-06-07 10:01:55 +0530

    Route to a random url

--- src/js/src/components/Board.js
+++ src/js/src/components/Board.js
@@ -15,8 +15,12 @@
  */
 
 import React from "react";
+import {useParams} from "react-router-dom"
 
 function Board() {
+
+    const {key} = useParams()
+
     return (
         <main className="main">
             <div className="button-container">
