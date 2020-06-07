9c9bf6b86b461812d17783efba67f7cb099bc779
Vignesh <emvignesh16@gmail.com>
2020-06-07 13:34:42 +0530

    Post save-content to the server

--- src/js/src/components/Board.js
+++ src/js/src/components/Board.js
@@ -23,6 +23,7 @@ function Board() {
 
     const [contentKey, setContentKey] = useState(key)
     const [content, setContent] = useState("")
+    const [saveStatus, setSaveStatus] = useState(0)
 
     async function getContent(contentKey) {
         const response = await fetch("http://localhost:8080/api/v1/save/" + contentKey)
@@ -44,12 +45,47 @@ function Board() {
         setContent(event.target.value)
     }
 
+    async function saveContent(contentKey, content) {
+        const requestBody = {
+            key: contentKey,
+            content,
+            expiresIn: 5
+        }
+
+        const response = await fetch("http://localhost:8080/api/v1/save/", {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+
+        if (response.status === 200) {
+            setSaveStatus(1)
+        } else {
+            setSaveStatus(2)
+        }
+    }
+
+    function handleSaveClick() {
+        setSaveStatus(0)
+        saveContent(contentKey, content)
+    }
+
     return (
         <main className="main">
             <div className="button-container">
-                <div className="button">Save It</div>
+                <div className="button" onClick={handleSaveClick}>Save It</div>
                 <div className="button">Copy It</div>
             </div>
+            {
+                saveStatus !== 0 &&
+                <p className="status-text">
+                    {saveStatus === 1 && "Successfully added!"}
+                    {saveStatus === 2 && "Something went wrong!"}
+                </p>
+
+            }
             <textarea className="content-text-area" value={content} onChange={handleTextChange}/>
         </main>
     )
